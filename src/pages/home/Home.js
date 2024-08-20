a280f0dc54f2995cfad44c8646a96500346056ac
Beyza Keser <beyzaaakeser@gmail.com>
2024-08-21 01:59:46 +0300

    first commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+
+import Topbar from '../../components/topbar/Topbar';
+import './home.css'
+
+import PersonIcon from '@mui/icons-material/Person';
+
+const Home = () => {
+  return (
+    <Topbar/>
+    
+  )
+}
+
+export default Home
