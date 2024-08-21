af61ad35cf676fc58c1a001807ad96b6a0c18e30
Beyza Keser <beyzaaakeser@gmail.com>
2024-08-21 17:13:21 +0300

    sidebar

---
+++ src/components/rigthbar/Rigthbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './rigthbar.css'
+
+
+const Rigthbar = () => {
+  return (
+    <div className='rigthbar'>Rigthbar</div>
+  )
+}
+
+export default Rigthbar
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './sidebar.css'
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+import ChatIcon from '@mui/icons-material/Chat';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import GroupIcon from '@mui/icons-material/Group';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import InsertInvitationIcon from '@mui/icons-material/InsertInvitation';
+import SchoolIcon from '@mui/icons-material/School';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="sidebarWrapper">
+            <ul className="sidebarList">
+                <li className="sidebarListItem">
+                    <RssFeedIcon className='sidebarIcon'/>
+                    <span className="sidebarListItemText">Feed</span>
+                </li>
+                <li className="sidebarListItem">
+                    <ChatIcon className='sidebarIcon'/>
+                    <span className="sidebarListItemText">Chat</span>
+                </li>
+                <li className="sidebarListItem">
+                    <PlayCircleIcon className='sidebarIcon'/>
+                    <span className="sidebarListItemText">Video</span>
+                </li>
+                <li className="sidebarListItem">
+                    <GroupIcon className='sidebarIcon'/>
+                    <span className="sidebarListItemText">Groups</span>
+                </li>
+                <li className="sidebarListItem">
+                    <BookmarkIcon className='sidebarIcon'/>
+                    <span className="sidebarListItemText">Bookmarks</span>
+                </li>
+                <li className="sidebarListItem">
+                    <HelpOutlineIcon className='sidebarIcon'/>
+                    <span className="sidebarListItemText">Questions</span>
+                </li>
+                <li className="sidebarListItem">
+                    <WorkOutlineIcon className='sidebarIcon'/>
+                    <span className="sidebarListItemText">Jobs</span>
+                </li>
+                <li className="sidebarListItem">
+                    <InsertInvitationIcon className='sidebarIcon'/>
+                    <span className="sidebarListItemText">Events</span>
+                </li>
+                <li className="sidebarListItem">
+                    <SchoolIcon className='sidebarIcon'/>
+                    <span className="sidebarListItemText">Courses</span>
+                </li>
+            </ul>
+
+            <button className='sidebarButton'>Show More</button>
+            <hr className='sidebarHr'  />
+            <ul className="sidebarFriendList">
+                <li className="sidebarFriend">
+                    <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+                    <span className='sidebarFriendName'>Jhon Doe</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
